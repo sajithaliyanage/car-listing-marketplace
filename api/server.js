1f97ab3e617ba5e2919335d62743d5b85bf07b54
sajithaliyanage <sajithaliyanage@gmail.com>
2023-12-12 14:28:36 +0300

    Configure security packages: helmet, xss-clean, morgan, and hpp for enhanced protection and logging

--- api/server.js
+++ api/server.js
@@ -1,12 +1,26 @@
 const cors = require('cors');
-const express = require('express');
 const colors = require('colors');
+const express = require('express');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const morgan = require('morgan');
+const xss = require('xss-clean');
 
 colors.enable();
 
 const app = express();
 
 app.use(cors());
+app.use(helmet());
+app.use(hpp());
+app.use(xss());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+} else {
+  console.log('Morgan logger set to developement mode'.yellow);
+  app.use(morgan('dev'));
+}
 
 const port = process.env.PORT;
 const host = process.env.HOST;
